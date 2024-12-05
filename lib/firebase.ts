f956f485f39bf45f0441b980a9c10156d3f33f1a
sofito-code <sofia.vanegasc@udea.edu.co>
2024-12-04 23:19:43 -0500

    local env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmgFyk2lpkZP0dxl1RMW9EFoa4Ya3gMSs",
-  authDomain: "iot-final-2024.firebaseapp.com",
-  projectId: "iot-final-2024",
-  storageBucket: "iot-final-2024.firebasestorage.app",
-  messagingSenderId: "997572165380",
-  appId: "1:997572165380:web:10f64a069e8feaa1ea9a51",
-  measurementId: "G-9KCD4W2JHZ"
-}
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
